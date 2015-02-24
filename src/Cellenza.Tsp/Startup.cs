51b7095947748c0ccd6f5796ce873fb63e73b4c0
clementpuell <clement.puell@cellenza.com>
2015-02-24 15:40:03 +0100

    - Intégration de Leaflet et jQuery - Chargement de données GeoJSON sur la carte

--- src/Cellenza.Tsp/Startup.cs
+++ src/Cellenza.Tsp/Startup.cs
@@ -1,14 +1,42 @@
-﻿using System;
-using Microsoft.AspNet.Builder;
-using Microsoft.AspNet.Http;
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.StaticFiles;
+using Microsoft.Framework.ConfigurationModel;
+using Microsoft.Framework.DependencyInjection;
+using Microsoft.Framework.Logging;
+using Microsoft.Framework.Logging.Console;
 
 namespace Cellenza.Tsp
 {
     public class Startup
     {
-        public void Configure(IApplicationBuilder app)
+        public Startup(IHostingEnvironment env)
         {
-            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+            // Setup configuration sources.
+            Configuration = new Configuration()
+                .AddEnvironmentVariables();
+        }
+
+        public IConfiguration Configuration { get; set; }
+
+        // This method gets called by the runtime.
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // Configure is called after ConfigureServices is called.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerfactory)
+        {
+            // Configure the HTTP request pipeline.
+            // Add the console logger.
+            loggerfactory.AddConsole();
+            
+            // Add static files to the request pipeline.
+            var options = new StaticFileOptions();
+            var contentType = new FileExtensionContentTypeProvider();
+            contentType.Mappings.Add(".json", "application/json");
+            options.ContentTypeProvider = contentType;
+            app.UseStaticFiles(options);
         }
     }
 }
---
+++ src/Cellenza.Tsp/wwwroot/js/tsp.js
@@ -0,0 +1,28 @@
+﻿/// <reference path="../lib/jquery/jquery.js" />
+/// <reference path="../leaflet/leaflet-src.js" />
+
+var map;
+
+window.onload = function () {
+    initMap();
+    loadGeoJsonData();
+}
+
+var initMap = function () {
+    // Cellenza
+    var startPosition = [48.866600401120644, 2.351692198135424];
+    // OpenStreetMaps
+    var osm = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+
+    map = L.map('map', {
+        center: startPosition,
+        zoom: 2,
+        layers: osm,
+    });
+}
+
+var loadGeoJsonData = function () {
+    $.getJSON('/cities.json', null, function (data) {
+        L.geoJson(data).addTo(map);
+    });
+}
